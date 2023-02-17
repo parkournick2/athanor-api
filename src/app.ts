6ef726e322474b4cc235afabc3577ed1a04d0830
Nicolas <nicolasalexandre0001@gmail.com>
2023-02-17 17:50:41 -0300

    setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv';
-import express, { Application, NextFunction, Request, Response } from 'express';
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
 
 dotenv.config();
 dotenv.config({ path: `.env.local`, override: true });
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Express server with TypeScript');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express server with TypeScript");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
