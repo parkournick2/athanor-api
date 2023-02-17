37aec11937e43eefa897ced3e2717e5efa42d47d
Nicolas <nicolasalexandre0001@gmail.com>
2023-02-17 16:36:44 -0300

    setup project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/ping', (req, res) => {
+  res.send('pong')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
