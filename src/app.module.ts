100f2a333c81fa7a324f97cd101442933056f6fe
Nicolas <nicolasalexandre0001@gmail.com>
2023-02-21 16:27:21 -0300

    setup prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
